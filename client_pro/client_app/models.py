d5e12d0466f78a9d9652cc513dbb95376dced51d
pj007-git <72685456+pj007-git@users.noreply.github.com>
2022-03-11 23:54:31 +0530

    Delete client_pro directory

--- client_pro/client_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class userDetails(models.Model):
-    roles = models.CharField(max_length=70, default='')
-    deleted = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
